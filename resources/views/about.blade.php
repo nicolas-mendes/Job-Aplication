217b3efb8b77e55306c38ab598681a028307e4e6
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-08-20 12:09:20 -0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, ad impedit, odit minima harum qui libero quae, magnam enim officiis ullam voluptas voluptate dicta adipisci quia pariatur error eaque labore.</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>Contact Page</h1>
+    <p>+55 (41)99999-1234</p>
+</x-layout>
