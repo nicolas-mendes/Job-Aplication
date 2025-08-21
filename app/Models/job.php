32adeafa6c73b6ad24e40569299dde2fba1a9d2a
Nicolas Mendes Fernandes <nicolasmendes@ufpr.br>
2025-08-21 14:06:28 -0300

    Faz o fetch das vagas direto da Database utilizando a biblioteca Illuminate

--- app/Models/job.php
+++ app/Models/job.php
@@ -2,61 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 /**
  * Class Job
  * This class provides methods for retrieving job data.
  */
-class Job
+class job extends Model
 {
-    /**
-     * Retrieves all job listings.
-     * @return array An array of job listings, where each job is an associative array
-     *               containing 'id', 'title', and 'salary'.
-     */
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'title' => 'FullStack',
-                'salary' => '3.500'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Frontend',
-                'salary' => '4.500'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Backend',
-                'salary' => '6.500'
-            ],
-            [
-                'id' => '4',
-                'title' => 'ProPlayer',
-                'salary' => '15.000'
-            ]
-        ];
-    }
-
-    /**
-     * Finds the first job with the give ID.
-     * @param int $id The ID of the job to find.
-     * @return array|null The job with the given ID, or null if not found.
-     */
-    public static function find($id)
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if(! $job) 
-        {
-            abort(404);
-        };
-        
-        return $job;
-    }
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary','empress','description'];
 }
 
 ?>
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,17 +1,15 @@
 <x-layout>
     <x-slot:heading>
-        Jobs Page
-    </x-slot:heading>
-
-    <h1>Jobs Available</h1>
-
+        Jobs Available
+    </x-slot:heading> 
 
 <ul>
     @foreach ($jobs as $job)
         <li>
-            <a href="/jobs/{{$job['id']}} ">
-                <strong>{{$job['title']}}</strong> <BR> Salary: R$ {{$job['salary']}}
+            <a href="/jobs/{{$job['id']}}" class="text-blue-300 hover:underline">
+                <strong>{{$job['title']}}</strong>
             </a>
+            <p>Salary: R${{$job['salary']}} <br> {{$job['description']}} </p>
         </li>
         <br>
     @endforeach
